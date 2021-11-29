df47b7ed456f279d0dbb7a7b5ab07007677a1ec1
ElenaLj <elenaljubijankic@gmail.com>
2021-11-29 16:18:12 +0100

    stampa oggetti

--- js/main.js
+++ js/main.js
@@ -1,9 +1,34 @@
 const app = new Vue({
     el: "#root",
     data: {
-
+       array: [
+           {
+            text: "Fare la spesa",
+            done: true,
+           },
+           {
+            text: "Portare il gatto dal veterinario",
+            done: false,
+           },
+           {
+            text: "Passare l'aspirapolvere",
+            done: true,
+           },
+           {
+            text: "Fare la lavatrice",
+            done: false,
+           },
+           {
+            text: "Portare la macchina dal meccanico",
+            done: true,
+           },
+           {
+            text: "Chiamare la mamma",
+            done: false,
+           },
+       ], 
     },
     methods: {
-        
+
     },
 });
